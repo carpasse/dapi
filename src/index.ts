34c3c654d44543abdec9935917a1d9dccf0ca180
Carlos Serrano <carpasse@gmail.com>
2024-01-29 10:29:50 +0100

    fix: fix library name references

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import DapiMixin from './DapiMixin';
 
 /**
- * @module @carpasse/dapis
+ * @module @carpasse/dapi
  */
 export type {DapiDefinition, DapiWrapper} from './DapiMixin';
 export {createDapi} from './createDapi';
