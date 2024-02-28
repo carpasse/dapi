022339faf4ad2228a4acc7ddc0ebfc26997e99ff
Carlos Serrano <carpasse@gmail.com>
2024-02-28 06:32:21 +0100

    fix(esm build): add extension to types import

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@
  * @module @carpasse/dapi
  */
 export {DapiMixin, type DapiDefinition, type DapiWrapper} from './DapiMixin.js';
-export type {DapiFns, DapiFn, DecoratorFn, HookFn} from './types';
+export type {DapiFns, DapiFn, DecoratorFn, HookFn} from './types/index.js';
 export {createDapi} from './createDapi.js';
